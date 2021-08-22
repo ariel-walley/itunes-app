6656170d5e02c9dc965dfb438a3a32d98b1dac0f
Ariel Walley <arielwalley95@gmail.com>
2021-08-22 02:05:36 -0400

    Create header component
    - Imported useState
    - Installed styled-components
    - Installed axios on front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Header from './header.js'
 
 function App() {
+  const [artist, setArtist] = useState(''); 
+
   useEffect(() => {  
     axios.get('http://localhost:4000/')
     .then((res) => {
       console.log(res.data.results);
+      if (res.data.results[0].wrapperType === 'artist') {
+        setArtist(res.data.results[0].artistName);
+      }
     })
     .catch((err) => {
       console.log(err)
@@ -15,6 +21,7 @@ function App() {
 
   return (
     <div>
+      <Header artistName={artist}/>
       <p>Lorem ipsum</p>
     </div>
   );
---
+++ client/src/header.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledHeader = styled.div`
+  text-align: center;
+`;
+
+export default function Header(props) {
+  return(
+    <StyledHeader>{props.artistName}</StyledHeader>
+  );
+}
